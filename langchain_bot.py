73089d8eaed90594208327d12d9c428b63bf8fc3
Aditi Sharma <aditiaditi2200@gmail.com>
2025-05-20 21:52:16 +0530

    Initial commit

---
+++ langchain_bot.py
@@ -0,0 +1,39 @@
+import streamlit as st
+from langchain.chains import LLMChain
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_openai import ChatOpenAI
+
+f=open('/Users/aditi/API_KEY/OPEN_AI_KEY.txt')
+open_api_key = f.read()
+
+import os
+os.environ['Open_api_key']=open_api_key
+
+st.title('I am Your Langchain AI....')
+
+st.chat_message("assistant").write("Hi, Welcome to our Bot.. How may I help you today...?")
+
+if "messages" not in st.session_state:
+    st.session_state["messages"]= []
+
+for msg in st.session_state["messages"]:
+    if isinstance(msg, dict) and "role" in msg and "content" in msg:
+        st.chat_message(msg["role"]).write(msg["content"])
+
+#Creating LLM
+
+chatgpt = ChatOpenAI(model_name ='gpt-4o-mini', api_key=open_api_key)
+
+query = st.chat_input("Enter your question:")
+
+prompt_template= ChatPromptTemplate.from_template("Answer the question in an easy, understandable way: {query}")
+llmchain_o=prompt_template | chatgpt
+
+if query:
+    st.session_state["messages"].append({"role":"user", "content": query})
+    response = llmchain_o.invoke({"query":query})
+    st.session_state["messages"].append({"role": "assistant", "content": response.content})
+    #print(response.content)
+    st.write(response.content)
+
+
